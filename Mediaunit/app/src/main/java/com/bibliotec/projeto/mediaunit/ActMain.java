683317f00e4c1712fb3ca5d087ad680728b00a63
Wictor13 <wictoralexandre93@gmail.com>
2016-09-28 16:06:13 -0300

    Calculadora media unit
    corrigir erros .

---
+++ Mediaunit/app/src/main/java/com/bibliotec/projeto/mediaunit/ActMain.java
@@ -0,0 +1,18 @@
+package com.bibliotec.projeto.mediaunit;
+
+/**
+ * Created by 5909 on 28/09/2016.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actmain);
+        //setContentView(R.layout.actmain);
+
+    }
+}
